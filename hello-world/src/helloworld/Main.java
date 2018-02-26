7180b0f56703bf62965dbe18cb43d1ea2d2273ce
Bojana Lučić <36507048+Bojana06@users.noreply.github.com>
2018-02-26 22:50:42 +0100

    Created the Hello World project

---
+++ hello-world/src/helloworld/Main.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("My name is Bojana.");
+
+	}
+
+}
